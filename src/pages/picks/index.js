e2b2234217ede35e37ba9f1e55ecc4d3776715a7
rickithadi <rickithadi@gmail.com>
2020-04-30 15:39:53 +0800

    sex

---
+++ src/pages/picks/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import Layout from '../../components/Layout';
+import GigRoll from '../../components/GigRoll';
+
+export default class pickIndexPage extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <div
+          className="full-width-image-container margin-top-10"
+          style={{
+            opacity: '0.8',
+            backgroundColor: 'pink',
+          }}>
+          <div className="text-center" style={{textAlign: 'center'}}>
+            <h1
+              className="has-text-weight-bold is-size-1"
+              style={{
+                boxShadow: '0.5rem 0 0 pink, -0.5rem 0 0 pink',
+                backgroundColor: 'pink',
+                color: 'white',
+                padding: '1rem',
+              }}>
+              Big Duck Pick
+              <hr
+                style={{
+                  height: '0.1em',
+                  color: 'yellow',
+                  background: 'yellow',
+                  border: 'none',
+                }}
+              />
+            </h1>
+            <p
+              style={{
+                boxShadow: '0.5rem 0 0 pink, -0.5rem 0 0 pink',
+                backgroundColor: 'pink',
+                color: 'black',
+                padding: '1rem',
+              }}>
+              Talk Cock sing song
+            </p>
+          </div>
+        </div>
+        <section className="section">
+          <div className="container">
+            <div className="content">
+              <GigRoll />
+            </div>
+          </div>
+        </section>
+      </Layout>
+    );
+  }
+}
