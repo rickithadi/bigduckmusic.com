08cc41a68b10b7d0dbaf3d3ff3a4a3d6c4db6c77
rickithadi <rickithadi@gmail.com>
2020-05-13 16:15:09 +0800

    carou wek

--- src/pages/gigs/index.js
+++ src/pages/gigs/index.js
@@ -1,35 +1,52 @@
 import React from 'react';
 
-//import * as pics from '../../img/carouPics';
-import logo from '../../img/social/facebook.svg';
+import uno from '../../img/carouPics/IMG_0529.jpg';
+import dos from '../../img/carouPics/IMG_1324.jpg';
+import tres from '../../img/carouPics/IMG_0522.jpg';
+import quatro from '../../img/carouPics/IMG_1474.jpg';
+import isnk from '../../img/carouPics/IMG_3455.jpg';
+import fux from '../../img/carouPics/IMG.jpg';
+import heh from '../../img/carouPics/img.jpg';
 import Carousel from 'react-bootstrap/Carousel';
 import Layout from '../../components/Layout';
 import GigRoll from '../../components/GigRoll';
 
 export default class gigIndexPage extends React.Component {
   render() {
+    const pics = [uno, dos, tres, quatro, isnk, fux, heh];
+    console.log(pics);
     return (
       <Layout>
-        <div className="full-width-image-container ">
-          <div
-            className="text-center"
-            style={{textAlign: 'center'}}
-            style={{
-              backgroundColor: '#a3b6de',
-              padding: '20px',
-              boxShadow: '0.5rem 0 0 #a3b6de, -0.5rem 0 0 #a3b6de',
-              borderRadius: '10px',
-            }}>
-            <h1
-              className=" head"
+        <Carousel>
+          {pics.map(i => {
+            console.log(i);
+            return (
+              <Carousel.Item>
+                <img src={i} />
+              </Carousel.Item>
+            );
+          })}
+          <div className="full-width-image-container ">
+            <div
+              className="text-center"
+              style={{textAlign: 'center'}}
               style={{
-                padding: '1rem',
+                backgroundColor: '#a3b6de',
+                padding: '20px',
+                boxShadow: '0.5rem 0 0 #a3b6de, -0.5rem 0 0 #a3b6de',
+                borderRadius: '10px',
               }}>
-              Big Duck Gigs
-            </h1>
-            <p className="usb">Our Main Events! And all our previous ones…</p>
+              <h1
+                className=" head"
+                style={{
+                  padding: '1rem',
+                }}>
+                Big Duck Gigs
+              </h1>
+              <p className="usb">Our Main Events! And all our previous ones…</p>
+            </div>
           </div>
-        </div>
+        </Carousel>
         <section className="section">
           <div className="container">
             <div className="content">
