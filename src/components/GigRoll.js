e4414a5c66b21a38040f140e5aeb568f0bde4504
rickithadi <rickithadi@gmail.com>
2020-05-14 15:26:42 +0800

    gig home

--- src/components/GigRoll.js
+++ src/components/GigRoll.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import {Link, graphql, StaticQuery} from 'gatsby';
 import PreviewCompatibleImage from './PreviewCompatibleImage';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Carousel from 'react-bootstrap/Carousel'
+import Carousel from 'react-bootstrap/Carousel';
 
 class GigRoll extends React.Component {
   render() {
@@ -14,47 +14,38 @@ class GigRoll extends React.Component {
       <div className="columns is-multiline">
         {posts &&
           posts.map(({node: post}) => (
-            <div className="is-parent column is-4" key={post.id}>
-              <article
-                style={{
-                  backgroundColor: 'pink',
-                  borderRadius: '10px',
+            <Link className="is-parent column is-4" to={post.fields.slug}>
+              <div key={post.id}>
+                <article
+                  style={{
+                    backgroundColor: '#a3b6de',
+                    borderRadius: '10px',
 
-                  border: '1px solid  #FF7500',
-                }}
-                className={`gig-list-item tile is-child box ${
-                  post.frontmatter.featuredpost ? 'is-featured' : ''
-                }`}>
-                <header>
-                  {post.frontmatter.featuredimage ? (
-                    <div className="featured-thumbnail">
-                      <PreviewCompatibleImage
-                        imageInfo={{
-                          image: post.frontmatter.featuredimage,
-                          alt: `featured image thumbnail for post ${post.frontmatter.title}`,
-                        }}
-                      />
-                    </div>
-                  ) : null}
-                  <Link className="title is-size-4" to={post.fields.slug}>
-                    <h1 className="othersub" >
-                      {post.frontmatter.title}
-                    </h1>
-                  </Link>
-                  <span className="subtitle is-size-5 is-block">
-                    {post.frontmatter.date}
-                  </span>
-                </header>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button" to={post.fields.slug}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </article>
-            </div>
+                    padding: '15px',
+                  }}
+                  className={`gig-list-item tile is-child box ${
+                    post.frontmatter.featuredpost ? 'is-featured' : ''
+                  }`}>
+                  <header>
+                    {post.frontmatter.featuredimage ? (
+                      <div className="featured-thumbnail">
+                        <PreviewCompatibleImage
+                          imageInfo={{
+                            image: post.frontmatter.featuredimage,
+                            alt: `featured image thumbnail for post ${post.frontmatter.title}`,
+                          }}
+                        />
+                      </div>
+                    ) : null}
+                    <p className="top">{post.frontmatter.title}</p>
+                    <span className="subtitle is-size-5 is-block">
+                      {post.frontmatter.date}
+                    </span>
+                  </header>
+                  <p style={{color: '#18305e'}}>{post.excerpt}</p>
+                </article>
+              </div>
+            </Link>
           ))}
       </div>
     );
