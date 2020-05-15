0bf95e6c4e0427b8e3eb2c2fe9e258d9d3c9b5b4
rickithadi <rickithadi@gmail.com>
2020-05-15 11:50:11 +0800

    moved caroupics to static page

--- src/pages/gigs/index.js
+++ src/pages/gigs/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
-import uno from '../../img/carouPics/IMG_0529.jpg';
-import dos from '../../img/carouPics/IMG_1324.jpg';
-import tres from '../../img/carouPics/IMG_0522.jpg';
-import quatro from '../../img/carouPics/IMG_1474.jpg';
-import isnk from '../../img/carouPics/IMG_3455.jpg';
-import fux from '../../img/carouPics/IMG.jpg';
-import heh from '../../img/carouPics/img.jpg';
+import uno from '../../../static/img/carouPics/IMG_0529.jpg';
+import dos from '../../../static/img/carouPics/IMG_1324.jpg';
+import tres from '../../../static/img/carouPics/IMG_0522.jpg';
+import quatro from '../../../static/img/carouPics/IMG_1474.jpg';
+import isnk from '../../../static/img/carouPics/IMG_3455.jpg';
+import fux from '../../../static/img/carouPics/IMG.jpg';
+import heh from '../../../static/img/carouPics/img.jpg';
 import Carousel from 'react-bootstrap/Carousel';
 import Layout from '../../components/Layout';
 import GigRoll from '../../components/GigRoll';
 
 export default class gigIndexPage extends React.Component {
   render() {
-    const pics = [uno, dos, tres, quatro, isnk, fux,heh];
+    const pics = [uno, dos, tres, quatro, isnk, fux, heh];
     console.log(pics);
     return (
       <Layout>
@@ -27,26 +27,26 @@ export default class gigIndexPage extends React.Component {
             );
           })}
         </Carousel>
-          <div className="full-width-image-container-gig">
-            <div
-              className="text-center"
-              style={{textAlign: 'center'}}
+        <div className="full-width-image-container-gig">
+          <div
+            className="text-center"
+            style={{textAlign: 'center'}}
+            style={{
+              backgroundColor: '#a3b6de',
+              padding: '20px',
+              boxShadow: '0.5rem 0 0 #a3b6de, -0.5rem 0 0 #a3b6de',
+              borderRadius: '10px',
+            }}>
+            <h1
+              className=" head"
               style={{
-                backgroundColor: '#a3b6de',
-                padding: '20px',
-                boxShadow: '0.5rem 0 0 #a3b6de, -0.5rem 0 0 #a3b6de',
-                borderRadius: '10px',
+                padding: '1rem',
               }}>
-              <h1
-                className=" head"
-                style={{
-                  padding: '1rem',
-                }}>
-                Big Duck Gigs
-              </h1>
-              <p className="usb">Our Main Events! And all our previous ones…</p>
-            </div>
+              Big Duck Gigs
+            </h1>
+            <p className="usb">Our Main Events! And all our previous ones…</p>
           </div>
+        </div>
         <section className="section">
           <div className="container">
             <div className="content">
