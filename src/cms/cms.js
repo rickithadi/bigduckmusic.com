6ae9bc31c232799cfd2477ca2990793184e68879
rickithadi <rickithadi@gmail.com>
2020-05-21 21:30:40 +0800

    cunt

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -24,23 +24,66 @@ CMS.registerEditorComponent({
   // Visible label
   label: 'Youtube',
   // Fields the user need to fill out when adding an instance of the component
-  fields: [{name: 'id', label: 'Youtube Video ID', widget: 'string'}],
+  fields: [
+    {name: 'id', label: 'Youtube Video ID', widget: 'string'},
+    {name: 'pic', label: 'pic', widget: 'image'},
+  ],
   // Pattern to identify a block as being an instance of this component
   pattern: /^youtube (\S+)$/,
   // Function to extract data elements from the regexp match
   fromBlock: function(match) {
     return {
       id: match[1],
+      pic: match[1],
     };
   },
   // Function to create a text block from an instance of this component
   toBlock: function(obj) {
-    return '<h1 style="color:red;text-align:center">' + obj.id + '</h1>';
+    let num = parseInt(obj.id);
+    let uno = num - 1;
+    let dos = num - 2;
+    let tres = num + 1;
+    let quat = num + 2;
+    console.log(quat);
+
+    return (
+      '<h1 style="color:grey;text-align:center">' +
+      dos +
+      ' ' +
+      uno +
+      ' <span style="color:orange;text-align:center"> ' +
+      obj.id +
+      '</span>' +
+      tres +
+      ' ' +
+      quat +
+      '</h1>'
+    );
   },
   // Preview output for this component. Can either be a string or a React component
   // (component gives better render performance)
   toPreview: function(obj) {
-    return '<h1 style="color:red">' + obj.id + '</h1>';
+   let num = parseInt(obj.id);
+    let uno = num - 1;
+    let dos = num - 2;
+    let tres = num + 1;
+    let quat = num + 2;
+    console.log(quat);
+
+    return (
+      '<h1 style="color:grey;text-align:center">' +
+      dos +
+      ' ' +
+      uno +
+      ' <span style="color:orange;text-align:center"> ' +
+      obj.id +
+      '</span>' +
+      tres +
+      ' ' +
+      quat +
+      '</h1>'
+    );
+
   },
 });
 CMS.registerEditorComponent({
