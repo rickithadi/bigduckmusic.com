5655d48c5bc764f6e0253617e87188a85ef92efa
rickithadi <rickithadi@gmail.com>
2020-07-18 21:25:07 +0800

    zoot

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import tape1 from '../img/sex/tape1.png';
 import heppy from '../img/sex/happ.png';
+import hap from '../img/sex/zoot.png';
 import tape2 from '../img/sex/tape2.png';
 import tape3 from '../img/sex/tape3.png';
 import {graphql} from 'gatsby';
@@ -32,13 +33,113 @@ export const AboutPageTemplate = ({
         minHeight: '100vh',
         backgroundColor: 'black',
       }}>
-      <section className="aboot " style={{height: '100%',paddingBottom:'50px'}}>
+      <section
+        className="is-hidden-desktop"
+        style={{height: '100%', paddingBottom: '50px'}}>
+        <div class="columns is-centered ">
+          <div class="column">
+            <h1 className="homeHeader" style={{padding: '20px'}}>
+              {title}
+            </h1>
+          </div>
+
+          <div class="column is-centered cen">
+            <img src={hap} className="cen" style={{marginTop: '-10vh',paddingBottom:'10vh'}} />
+          </div>
+        </div>
+        <div class="columns is-vcentered ">
+          <div
+            class="column is-6 is-offset-5 is-vcentered chatty
+            "
+            style={{
+              minHeight: '100%',
+              color: 'black',
+              padding: '0px',
+              height: '100%',
+            }}>
+            <div
+              class="card
+              ">
+              <img src={tape1} style={{marginTop: '-100px'}} className="cen" />
+              <div class="container">
+                <h1 className="bigboi">{header1}</h1>
+
+                <p style={{paddingBottom: '15px'}}>{body1}</p>
+                <p style={{paddingBottom: '15px'}}>{body1_2}</p>
+              </div>
+              <div
+                class="card-image"
+                style={{
+                  margin: '0 !important',
+                  padding: '0px !important',
+                  objectFit: 'cover',
+                }}>
+                <img src={heppy} />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="columns  ">
+          <div
+            class="bike             "
+            style={{
+              minHeight: '100%',
+              color: 'black',
+              padding: '0px',
+              height: '100%',
+            }}>
+            <div
+              style={{
+                backgroundColor: '#ADFE29',
+                paddingBottom: '12vh',
+                paddingBottom: '12vh',
+              }}
+              class="card">
+              <img src={tape2} className="cen" style={{marginTop: '-90px'}} />
+              <div
+                class="container"
+                style={{
+                  backgroundColor: '#ADFE29',
+                }}>
+                <h1 className="bigboi">{header2}</h1>
+
+                <p style={{paddingBottom: '15px'}}>{body2}</p>
+                <p style={{paddingBottom: '15px'}}>{body2_2}</p>
+                <p style={{paddingBottom: '15px'}}>{body2_3}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="columns  ">
+          <div
+            class="bike"
+            style={{
+              minHeight: '100%',
+              position: 'relative',
+              paddingBottom: '10vh',
+              paddingRight: '10px',
+              color: 'black',
+              height: '100%',
+              backgroundColor: '#FBFF30',
+            }}>
+            <img src={tape3} className="tapeSmol cen" />
+            <div class="container">
+              <h1 className="bigboi">{header3}</h1>
+              <p style={{paddingBottom: '15px'}}>{body3}</p>
+              <p style={{paddingBottom: '15px'}}>{body3_2}</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section
+        className="aboot is-hidden-touch
+"
+        style={{height: '100%', paddingBottom: '50px'}}>
         <h1 className="homeHeader" style={{padding: '20px'}}>
           {title}
         </h1>
-        <div
-          class="columns  is-hidden-touch is-vcentered "
-          style={{paddingTop: '5vh'}}>
+        <div class="columns is-vcentered " style={{paddingTop: '5vh'}}>
           <div
             class="column is-6 is-offset-5 is-vcentered chatty
             "
@@ -48,7 +149,10 @@ export const AboutPageTemplate = ({
               color: 'black',
               height: '100%',
             }}>
-            <div class="card">
+            <div
+              class="card
+            is-hidden-touch
+              ">
               <img
                 src={tape1}
                 className="tape cen"
@@ -73,8 +177,7 @@ export const AboutPageTemplate = ({
           </div>
         </div>
         <div
-          class="column is-5 is-offset-1 is-vcentered bike is-hidden-touch
-            "
+          class="column is-5 is-offset-1 is-vcentered bike             "
           style={{
             minHeight: '100%',
             paddingBottom: '10vh',
@@ -97,9 +200,9 @@ export const AboutPageTemplate = ({
               }}>
               <h1 className="bigboi">{header2}</h1>
 
-              <p style={{paddingBottom:'15px'}}>{body2}</p>
-              <p style={{paddingBottom:'15px'}} >{body2_2}</p>
-              <p style={{paddingBottom:'15px'}} >{body2_3}</p>
+              <p style={{paddingBottom: '15px'}}>{body2}</p>
+              <p style={{paddingBottom: '15px'}}>{body2_2}</p>
+              <p style={{paddingBottom: '15px'}}>{body2_3}</p>
             </div>
           </div>
         </div>
@@ -119,13 +222,13 @@ export const AboutPageTemplate = ({
             <img src={tape3} className="tapeSmol cen" />
             <div class="container">
               <h1 className="bigboi">{header3}</h1>
-              <p  style={{paddingBottom:'15px'}}>{body3}</p>
-              <p  style={{paddingBottom:'15px'}}>{body3_2}</p>
+              <p style={{paddingBottom: '15px'}}>{body3}</p>
+              <p style={{paddingBottom: '15px'}}>{body3_2}</p>
             </div>
           </div>
         </div>
       </section>
-      <section style={{backgroundColor: 'white'}}>hello</section>
+      <section style={{backgroundColor: 'white'}}>contact form</section>
     </div>
   );
 };
