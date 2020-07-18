bc373ad7eaad010765228db91e6ee518591a21bf
rickithadi <rickithadi@gmail.com>
2020-07-18 14:28:31 +0800

    fk

--- src/pages/about/index.md
+++ src/pages/about/index.md
@@ -2,14 +2,23 @@
 templateKey: about-page
 path: /about
 title: About Big Duck
+header1: BIG DUCK IS A PASSIONATE COLLECTIVE OF MUSIC ENTHUSIASTS AND GI PROMOTERS, PROUD TO CELEBRATE ALL THINGS GOOD ABOUT MUSIC AND THEIR LIVE EXPERIENCE
+header2: About Big Duck
+header3: About Big Duck
+body1: Beginning our journey in mid-2019 with Naybeats, we have since followed with a series of fully-packed gigs, featuring carefully curated lineups with artists and arrangements both unorthodox and varied. Always seeking to bring an eclectic mix of both well-loved scene veterans and fresh up-and-comers with potential; we deliver high energy and exciting shows, where every event is an unforgettable experience for our audiences (and artists as well!).
+body2: About Big Duck
+body3: About Big Duck
 ---
+
 ### Who are we?
+
+motherufk
 
 Big Duck is a passionate collective of music enthusiasts and gig promoters, proud to celebrate all things good about music and their live experiences! Beginning our journey in mid-2019 with Naybeats, we have since followed with a series of fully-packed gigs, featuring carefully curated lineups with artists and arrangements both unorthodox and varied. Always seeking to bring an eclectic mix of both well-loved scene veterans and fresh up-and-comers with potential; we deliver high energy and exciting shows, where every event is an unforgettable experience for our audiences (and artists as well!).
 
 ### What is This?
 
-Over time, we felt that this passion didn’t have to be one that stopped at shows - so here at our site, we have built another space to celebrate our music in its different forms. We’ll highlight the tunes and bands our team is passionate about, talk to artists about the music that inspires them and their journeys, give our opinions on the latest releases and entertain you with our columns and everything that lay in between. We look forward to highlight all interesting music across all that genres that lay between! 
+Over time, we felt that this passion didn’t have to be one that stopped at shows - so here at our site, we have built another space to celebrate our music in its different forms. We’ll highlight the tunes and bands our team is passionate about, talk to artists about the music that inspires them and their journeys, give our opinions on the latest releases and entertain you with our columns and everything that lay in between. We look forward to highlight all interesting music across all that genres that lay between!
 
 ### Our Aim.
 
--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,34 +1,109 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import tape1 from '../img/sex/tape1.png';
+import heppy from '../img/sex/happ.png';
+import tape2 from '../img/sex/tape2.png';
+import tape3 from '../img/sex/tape3.png';
 import {graphql} from 'gatsby';
 import Layout from '../components/Layout';
 import Content, {HTMLContent} from '../components/Content';
 
-export const AboutPageTemplate = ({title, content, contentComponent}) => {
+export const AboutPageTemplate = ({
+  title,
+  content,
+  header1,
+  header2,
+  header3,
+  body1,
+  body2,
+  body3,
+  contentComponent,
+}) => {
   const PageContent = contentComponent || Content;
+  console.log('bro', header1);
 
   return (
-    <section className="section aboot">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section" style={{padding: '0'}}>
-              <p className="head" style={{paddingBottom: '10px'}}>
-                {title}
-              </p>
-              <div
-                style={{
-                  backgroundColor: 'grey',
-                  padding: '15px',
-                }}>
-                <PageContent className="content biker" content={content} />
-              </div>
+    <div
+      style={{minHeight: '100vh'}}
+      style={{
+        backgroundColor: 'black',
+      }}>
+      <section className="aboot" style={{minHeight: '100%'}}>
+        <h1 className="homeHeader" style={{padding: '20px'}}>
+          {title}
+        </h1>
+        <div class="columns is-vcentered " style={{paddingTop: '15vh'}}>
+          <div
+            class="column is-6 is-offset-5 is-vcentered chatty
+            "
+            style={{
+              minHeight: '100%',
+              paddingBottom: '10vh',
+              color: 'black',
+              height: '100%',
+            }}>
+                    <div class="card">
+            <img
+              src={tape1}
+              className="tape cen"
+              style={{paddingTop: '15vh'}}
+            />
+            <div class="container">
+              <h1 className="bigboi">{header1}</h1>
+
+              <p>{body1}</p>
+            </div>
+            <div
+              class="card-image"
+              style={{
+                margin: '0 !important',
+                padding: '0px !important',
+                objectFit: 'cover',
+              }}>
+              <img src={heppy} />
             </div>
           </div>
         </div>
-      </div>
-      <div style={{background: 'transparent',height:'30vh'}}>vova</div>
-    </section>
+        </div>
+        <div class="columns is-vcentered " style={{paddingTop: '45vh'}}>
+          <div
+            class="column is-6  is-vcentered chatty
+            "
+            style={{
+              minHeight: '100%',
+              paddingBottom: '10vh',
+              color: 'black',
+              height: '100%',
+              backgroundColor: 'green',
+            }}>
+            <img src={tape2} className="tape cen" />
+            <div class="container">
+              <h1 className="bigboi">{header2}</h1>
+
+              <p>{body2}</p>
+            </div>
+          </div>
+        </div>
+        <div class="columns is-vcentered " style={{paddingTop: '5vh'}}>
+          <div
+            class="column is-4 is-offset-7 is-pulled-right chatty"
+            style={{
+              minHeight: '100%',
+              paddingBottom: '10vh',
+              color: 'black',
+              height: '100%',
+              backgroundColor: 'yellow',
+            }}>
+            <img src={tape3} className="tapeSmol cen" />
+            <div class="container">
+              <h1 className="bigboi">{header3}</h1>
+
+              <p>{body3}</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 };
 
@@ -46,6 +121,12 @@ const AboutPage = ({data}) => {
       <AboutPageTemplate
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
+        header1={post.frontmatter.header1}
+        header2={post.frontmatter.header2}
+        header3={post.frontmatter.header3}
+        body1={post.frontmatter.body1}
+        body2={post.frontmatter.body2}
+        body3={post.frontmatter.body3}
         content={post.html}
       />
     </Layout>
@@ -64,6 +145,12 @@ export const aboutPageQuery = graphql`
       html
       frontmatter {
         title
+        header1
+        header2
+        header3
+        body1
+        body2
+        body3
       }
     }
   }
