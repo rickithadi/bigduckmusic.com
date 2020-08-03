aa766350aaff65018c2bbe6dc7f712f1cf362d63
rickithadi <rickithadi@gmail.com>
2020-08-03 23:22:01 +0800

    sex

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -25,202 +25,68 @@ export const AboutPageTemplate = ({
   console.log('bro', body1_2);
 
   return (
-    <div
-      style={{
-        minHeight: '100vh',
-        backgroundColor: 'black',
-        color: 'black',
-      }}>
-      <section
-        className="is-hidden-desktop"
-        style={{height: '100%', paddingBottom: '50px'}}>
-        <div class="columns is-centered ">
-          <div class="column">
-            <h1 className="homeHeader" style={{padding: '20px'}}>
-              {title}
-            </h1>
-          </div>
-
-          <div class="column is-centered ">
-            <img
-              src={hap}
-              className="cen"
-              style={{marginTop: '-5vh', paddingBottom: '10vh'}}
-            />
-          </div>
-        </div>
-        <div class="columns is-vcentered ">
-          <div
-            class="column is-6 is-offset-5 is-vcentered
-            "
-            style={{
-              minHeight: '100%',
-              color: 'black',
-              padding: '0px',
-              height: '100%',
-            }}>
-            <div
-              class="card
-              ">
-              <img
-                src={tape1}
-                style={{marginTop: '-100px', maxWidth: '90%'}}
-                className="cen"
-              />
-              <div class="container">
-                <h1 className="idiot">{header1}</h1>
-
-                <p
-                  className="bike"
-                  style={{
-                    paddingBottom: '15px',
-
-                    color: 'black',
-                  }}>
-                  {body1}
-                </p>
-                <p
-                  className="bike"
-                  style={{
-                    paddingBottom: '15px',
-
-                    color: 'black',
-                  }}>
-                  {body1_2}
-                </p>
-              </div>
-              <div
-                class="card-image"
-                style={{
-                  margin: '0 !important',
-                  padding: '0px !important',
-                  objectFit: 'cover',
-                }}>
-                <img src={heppy} />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="columns  ">
-          <div
-            class="bike"
-            style={{
-              minHeight: '100%',
-              color: 'black',
-              padding: '0px',
-              height: '100%',
-            }}>
-            <div
-              style={{
-                backgroundColor: '#ADFE29',
-                paddingBottom: '12vh',
-                paddingBottom: '12vh',
-              }}
-              class="card">
-              <img src={tape2} className="cen" style={{marginTop: '-90px'}} />
-              <div
-                class="container"
-                style={{
-                  backgroundColor: '#ADFE29',
-                }}>
-                <h1 className="idiot">{header2}</h1>
-                <h1 className="idiot">{header2}</h1>
-                <h1 className="idiot">{header2}</h1>
-
-                <p
-                  style={{
-                    paddingBottom: '15px',
-                    color: 'black',
-                  }}
-                  className="bike">
-                  {body2}
-                </p>
-                <p
-                  style={{
-                    paddingBottom: '15px',
-                    color: 'black',
-                  }}
-                  className="bike">
-                  {body2_2}
-                </p>
-                <p
-                  style={{
-                    paddingBottom: '15px',
-                    color: 'black',
-                  }}
-                  className="bike">
-                  {body2_3}
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="columns  ">
-          <div
-            class="bike"
-            style={{
-              minHeight: '100%',
-              position: 'relative',
-              paddingBottom: '10vh',
-              paddingRight: '10px',
-              color: 'black',
-              height: '100%',
-              backgroundColor: '#FBFF30',
-            }}>
-            <img src={tape3} className="tapeSmol cen" />
-            <div class="container">
-              <h1 className="idiot">{header3}</h1>
-              <p style={{paddingBottom: '15px'}} className="bike">
-                {body3}
-              </p>
-              <p style={{paddingBottom: '15px'}} className="bike">
-                {body3_2}
-              </p>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <div style={{background: 'black'}} className=" is-hidden-touch  ">
-        <h1 className="homeHeader  " style={{padding: '20px'}}>
-          {title}
-        </h1>
-      </div>
-      <section
-        className="is-hidden-touch "
+    <Layout>
+      <div
         style={{
-          height: '100%',
-          paddingBottom: '50px',
+          minHeight: '100vh',
+          backgroundColor: 'black',
+          color: 'black',
         }}>
-        <div class="cunty" style={{marginTop: '5vh'}}>
-          <div class="columns is-vcentered " style={{marginTop: '5vh'}}>
-            <div class="column is-5 is-centered"></div>
+        <section
+          className="is-hidden-desktop"
+          style={{height: '100%', paddingBottom: '50px'}}>
+          <div class="columns is-centered ">
+            <div class="column">
+              <h1 className="homeHeader" style={{padding: '20px'}}>
+                {title}
+              </h1>
+            </div>
 
+            <div class="column is-centered ">
+              <img
+                src={hap}
+                className="cen"
+                style={{marginTop: '-5vh', paddingBottom: '10vh'}}
+              />
+            </div>
+          </div>
+          <div class="columns is-vcentered ">
             <div
-              class="column is-6 is-vcentered
+              class="column is-6 is-offset-5 is-vcentered
             "
               style={{
                 minHeight: '100%',
-                paddingBottom: '10vh',
                 color: 'black',
+                padding: '0px',
                 height: '100%',
               }}>
               <div
                 class="card
-            is-hidden-touch
               ">
                 <img
                   src={tape1}
-                  className="tape cen"
-                  style={{paddingTop: '15vh'}}
+                  style={{marginTop: '-100px', maxWidth: '90%'}}
+                  className="cen"
                 />
-                <div class="container bike">
-                  <h1 className="idiot bigboi">{header1}</h1>
+                <div class="container">
+                  <h1 className="idiot">{header1}</h1>
 
-                  <p style={{paddingBottom: '15px'}} className="bike">
+                  <p
+                    className="bike"
+                    style={{
+                      paddingBottom: '15px',
+
+                      color: 'black',
+                    }}>
                     {body1}
                   </p>
-                  <p style={{paddingBottom: '15px'}} className="bike">
+                  <p
+                    className="bike"
+                    style={{
+                      paddingBottom: '15px',
+
+                      color: 'black',
+                    }}>
                     {body1_2}
                   </p>
                 </div>
@@ -230,60 +96,70 @@ export const AboutPageTemplate = ({
                     margin: '0 !important',
                     padding: '0px !important',
                     objectFit: 'cover',
-                    width: '100% !important',
                   }}>
-                  <img src={heppy} style={{maxWidth: 'none', width: '100%'}} />
+                  <img src={heppy} />
                 </div>
               </div>
             </div>
           </div>
-          <div
-            class="column is-5 is-offset-1 is-vcentered bike "
-            style={{
-              minHeight: '100%',
-              paddingBottom: '10vh',
-              padding: '20px',
-              marginTop: '-20vh',
-              position: 'relative',
-              color: 'black',
-              height: '100%',
-            }}>
+          <div class="columns  ">
             <div
+              class="bike"
               style={{
-                backgroundColor: '#ADFE29',
-              }}
-              class="card">
-              <img src={tape2} className="tape cen" />
+                minHeight: '100%',
+                color: 'black',
+                padding: '0px',
+                height: '100%',
+              }}>
               <div
-                class="container bike"
                 style={{
                   backgroundColor: '#ADFE29',
-                  color: 'black',
-                  marginTop: '-50px',
-                }}>
-                <h1 className="big">{header2}</h1>
-                <h1 className="big">{header2}</h1>
-                <h1 className="big">{header2}</h1>
+                  paddingBottom: '12vh',
+                  paddingBottom: '12vh',
+                }}
+                class="card">
+                <img src={tape2} className="cen" style={{marginTop: '-90px'}} />
+                <div
+                  class="container"
+                  style={{
+                    backgroundColor: '#ADFE29',
+                  }}>
+                  <h1 className="idiot">{header2}</h1>
+                  <h1 className="idiot">{header2}</h1>
+                  <h1 className="idiot">{header2}</h1>
 
-                <p
-                  style={{paddingTop: '35px', paddingBottom: '15px'}}
-                  className="bike">
-                  {body2}
-                </p>
-                <p style={{paddingBottom: '15px'}} className="bike">
-                  {body2_2}
-                </p>
-                <p style={{paddingBottom: '15px'}} className="bike">
-                  {body2_3}
-                </p>
+                  <p
+                    style={{
+                      paddingBottom: '15px',
+                      color: 'black',
+                    }}
+                    className="bike">
+                    {body2}
+                  </p>
+                  <p
+                    style={{
+                      paddingBottom: '15px',
+                      color: 'black',
+                    }}
+                    className="bike">
+                    {body2_2}
+                  </p>
+                  <p
+                    style={{
+                      paddingBottom: '15px',
+                      color: 'black',
+                    }}
+                    className="bike">
+                    {body2_3}
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-          <div class="columns is-hidden-touch ">
+          <div class="columns  ">
             <div
-              class="column is-3 is-offset-7 is-pulled-right bike"
+              class="bike"
               style={{
-                marginTop: '-27vh',
                 minHeight: '100%',
                 position: 'relative',
                 paddingBottom: '10vh',
@@ -293,23 +169,152 @@ export const AboutPageTemplate = ({
                 backgroundColor: '#FBFF30',
               }}>
               <img src={tape3} className="tapeSmol cen" />
-              <div class="container bike">
-                <h1 className="bigboi idiot">{header3}</h1>
-                <p className="bike" style={{paddingBottom: '15px'}}>
+              <div class="container">
+                <h1 className="idiot">{header3}</h1>
+                <p style={{paddingBottom: '15px'}} className="bike">
                   {body3}
                 </p>
-                <p className="bike" style={{paddingBottom: '15px'}}>
+                <p style={{paddingBottom: '15px'}} className="bike">
                   {body3_2}
                 </p>
               </div>
             </div>
-          </div>{' '}
+          </div>
+        </section>
+
+        <div style={{background: 'black'}} className=" is-hidden-touch  ">
+          <h1 className="homeHeader  " style={{padding: '20px'}}>
+            {title}
+          </h1>
         </div>
-      </section>
-      <section style={{backgroundColor: 'white'}}>
-        <Index />
-      </section>
-    </div>
+        <section
+          className="is-hidden-touch "
+          style={{
+            height: '100%',
+            paddingBottom: '50px',
+          }}>
+          <div class="cunty" style={{marginTop: '5vh'}}>
+            <div class="columns is-vcentered " style={{marginTop: '5vh'}}>
+              <div class="column is-5 is-centered"></div>
+
+              <div
+                class="column is-6 is-vcentered
+            "
+                style={{
+                  minHeight: '100%',
+                  paddingBottom: '10vh',
+                  color: 'black',
+                  height: '100%',
+                }}>
+                <div
+                  class="card
+            is-hidden-touch
+              ">
+                  <img
+                    src={tape1}
+                    className="tape cen"
+                    style={{paddingTop: '15vh'}}
+                  />
+                  <div class="container bike">
+                    <h1 className="idiot bigboi">{header1}</h1>
+
+                    <p style={{paddingBottom: '15px'}} className="bike">
+                      {body1}
+                    </p>
+                    <p style={{paddingBottom: '15px'}} className="bike">
+                      {body1_2}
+                    </p>
+                  </div>
+                  <div
+                    class="card-image"
+                    style={{
+                      margin: '0 !important',
+                      padding: '0px !important',
+                      objectFit: 'cover',
+                      width: '100% !important',
+                    }}>
+                    <img
+                      src={heppy}
+                      style={{maxWidth: 'none', width: '100%'}}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div
+              class="column is-5 is-offset-1 is-vcentered bike "
+              style={{
+                minHeight: '100%',
+                paddingBottom: '10vh',
+                padding: '20px',
+                marginTop: '-20vh',
+                position: 'relative',
+                color: 'black',
+                height: '100%',
+              }}>
+              <div
+                style={{
+                  backgroundColor: '#ADFE29',
+                }}
+                class="card">
+                <img src={tape2} className="tape cen" />
+                <div
+                  class="container bike"
+                  style={{
+                    backgroundColor: '#ADFE29',
+                    color: 'black',
+                    marginTop: '-50px',
+                  }}>
+                  <h1 className="big">{header2}</h1>
+                  <h1 className="big">{header2}</h1>
+                  <h1 className="big">{header2}</h1>
+
+                  <p
+                    style={{paddingTop: '35px', paddingBottom: '15px'}}
+                    className="bike">
+                    {body2}
+                  </p>
+                  <p style={{paddingBottom: '15px'}} className="bike">
+                    {body2_2}
+                  </p>
+                  <p style={{paddingBottom: '15px'}} className="bike">
+                    {body2_3}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="columns is-hidden-touch ">
+              <div
+                class="column is-3 is-offset-7 is-pulled-right bike"
+                style={{
+                  marginTop: '-27vh',
+                  minHeight: '100%',
+                  position: 'relative',
+                  paddingBottom: '10vh',
+                  paddingRight: '10px',
+                  color: 'black',
+                  height: '100%',
+                  backgroundColor: '#FBFF30',
+                }}>
+                <img src={tape3} className="tapeSmol cen" />
+                <div class="container bike">
+                  <h1 className="bigboi idiot">{header3}</h1>
+                  <p className="bike" style={{paddingBottom: '15px'}}>
+                    {body3}
+                  </p>
+                  <p className="bike" style={{paddingBottom: '15px'}}>
+                    {body3_2}
+                  </p>
+                </div>
+              </div>
+            </div>{' '}
+          </div>
+        </section>
+        <section style={{backgroundColor: 'white'}}>
+          <Index />
+        </section>
+      </div>
+    </Layout>
   );
 };
 
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -13,161 +13,165 @@ import picks from '../img/sex/picks.svg';
 import tra from '../img/sex/trans.png';
 
 export const IndexPageTemplate = ({title, subheading}) => (
-  <div style={{minHeight: '100%'}}>
-    <section
-      style={{
-        backgroundColor: 'black',
-        paddingTop: '2vh',
-        paddingBottom: '20vh',
-      }}
-      className="home">
-      <div class="columns is-vcentered is-mobile">
-        <div
-          class="column is-6 is-offset-1 is-vcentered"
-          style={{minHeight: '100%', paddingBottom: '10vh'}}>
-          <p
-            className="homeHeader"
-            style={{
-              textAlign: 'left !important',
-              alignItems: 'left',
-            }}>
-            {subheading}
-          </p>
-        </div>
-      </div>
-    </section>
-    <img class="is-hidden" src={hap} style={{paddingLeft: '50%'}} />
-
-    <section
-      className="topek hero is-large  "
-      style={{minHeight: '100vh', paddingTop: '5vh'}}>
-      <div
-        class="columns is-vcentered is-mobile is-hidden-desktop  "
-        style={{marginTop: '-250px'}}>
-        <div class="columns is-vcentered is-mobile" style={{marginTop: '5px'}}>
-          <div class="column  is-vcentered">
-            <img
-              class="is-vcentered cen"
-              src={arrow}
-              style={{paddingLeft: '50%'}}
-            />
-
-            <img src={cunt} style={{paddingLeft: '10%'}} />
-          </div>
-        </div>
-      </div>
-
-      <div
-        class="columns is-vcentered is-hidden-touch  "
-        style={{marginTop: '20px'}}>
-        <div
-          class="columns is-vcentered is-mobile"
-          style={{marginTop: '-250px'}}>
-          <div class="column  is-vcentered">
-            <img src={cunt} />
-          </div>
-        </div>
-      </div>
-
-      <div
-        className="hero-body"
-        style={{padding: '10px', paddingBottom: '20px', marginTop: '-30px'}}>
-        <div className="container">
-          <BlogRollF />
-        </div>
-        <div style={{textAlign: 'center', paddingTop: '5vh'}}>
-          <Link
-            to="/features"
-            class="button "
-            style={{backgroundColor: 'black', color: 'white'}}>
-            Read More{' '}
-          </Link>
-        </div>
-      </div>
-    </section>
-
-    <section style={{backgroundColor: 'black'}}>
-      <img
-        src={picks}
-        style={{width: '100%', maxWidth: 'none'}}
-        class="is-hidden-touch"
-      />
-      <div
-        class="is-hidden-desktop"
-        style={{width: '100vw', overflow: 'hidden'}}>
-        <img src={picks} style={{height: '100%', maxWidth: 'none'}} />
-      </div>
-      <div style={{height: '100%'}}>
-        <PickRoll />
-      </div>
-      <div style={{height: '100%', background: '#000000'}}>
-        <img style={{width: '100vw'}} class="is-vcentered cen" src={tra} />
-      </div>
-      <div className="contribute">
-        <div class="columns is-vcentered ">
+  <Layout>
+    <div style={{minHeight: '100%'}}>
+      <section
+        style={{
+          backgroundColor: 'black',
+          paddingTop: '2vh',
+          paddingBottom: '20vh',
+        }}
+        className="home">
+        <div class="columns is-vcentered is-mobile">
           <div
-            class="column  is-offset-1 is-vcentered"
-            style={{minHeight: '100%'}}>
-            <div class="columns ">
-              <div
-                class="column  is-pulled-right is-hidden-desktop"
-                style={{paddingRight: '10vw'}}>
-                <span className="dot "></span>
-                <span className="doti"></span>
-              </div>
-              <div class="column is-mobile is-vcentered is-offset-1">
-                <h3 className="head" style={{padding: '10px'}}>
-                  want to contribute?
-                </h3>
-                <div class="columns ">
-                  <div
-                    class="column is-pulled-right is-hidden-touch"
-                    style={{paddingLeft: '46px', paddingTop: '26px'}}>
-                    <span className="dot "></span>
-                    <span className="doti"></span>
+            class="column is-6 is-offset-1 is-vcentered"
+            style={{minHeight: '100%', paddingBottom: '10vh'}}>
+            <p
+              className="homeHeader"
+              style={{
+                textAlign: 'left !important',
+                alignItems: 'left',
+              }}>
+              {subheading}
+            </p>
+          </div>
+        </div>
+      </section>
+      <img class="is-hidden" src={hap} style={{paddingLeft: '50%'}} />
+
+      <section
+        className="topek hero is-large  "
+        style={{minHeight: '100vh', paddingTop: '5vh'}}>
+        <div
+          class="columns is-vcentered is-mobile is-hidden-desktop  "
+          style={{marginTop: '-250px'}}>
+          <div
+            class="columns is-vcentered is-mobile"
+            style={{marginTop: '5px'}}>
+            <div class="column  is-vcentered">
+              <img
+                class="is-vcentered cen"
+                src={arrow}
+                style={{paddingLeft: '50%'}}
+              />
+
+              <img src={cunt} style={{paddingLeft: '10%'}} />
+            </div>
+          </div>
+        </div>
+
+        <div
+          class="columns is-vcentered is-hidden-touch  "
+          style={{marginTop: '20px'}}>
+          <div
+            class="columns is-vcentered is-mobile"
+            style={{marginTop: '-250px'}}>
+            <div class="column  is-vcentered">
+              <img src={cunt} />
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="hero-body"
+          style={{padding: '10px', paddingBottom: '20px', marginTop: '-30px'}}>
+          <div className="container">
+            <BlogRollF />
+          </div>
+          <div style={{textAlign: 'center', paddingTop: '5vh'}}>
+            <Link
+              to="/features"
+              class="button "
+              style={{backgroundColor: 'black', color: 'white'}}>
+              Read More{' '}
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <section style={{backgroundColor: 'black'}}>
+        <img
+          src={picks}
+          style={{width: '100%', maxWidth: 'none'}}
+          class="is-hidden-touch"
+        />
+        <div
+          class="is-hidden-desktop"
+          style={{width: '100vw', overflow: 'hidden'}}>
+          <img src={picks} style={{height: '100%', maxWidth: 'none'}} />
+        </div>
+        <div style={{height: '100%'}}>
+          <PickRoll />
+        </div>
+        <div style={{height: '100%', background: '#000000'}}>
+          <img style={{width: '100vw'}} class="is-vcentered cen" src={tra} />
+        </div>
+        <div className="contribute">
+          <div class="columns is-vcentered ">
+            <div
+              class="column  is-offset-1 is-vcentered"
+              style={{minHeight: '100%'}}>
+              <div class="columns ">
+                <div
+                  class="column  is-pulled-right is-hidden-desktop"
+                  style={{paddingRight: '10vw'}}>
+                  <span className="dot "></span>
+                  <span className="doti"></span>
+                </div>
+                <div class="column is-mobile is-vcentered is-offset-1">
+                  <h3 className="head" style={{padding: '10px'}}>
+                    want to contribute?
+                  </h3>
+                  <div class="columns ">
+                    <div
+                      class="column is-pulled-right is-hidden-touch"
+                      style={{paddingLeft: '46px', paddingTop: '26px'}}>
+                      <span className="dot "></span>
+                      <span className="doti"></span>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div
-            class="column is-4 is-offset-1 is-vcentered"
-            style={{
-              minHeight: '100%',
-              paddingLeft: '29px',
-              color: 'black',
-            }}>
-            <p className="bike">
-              Like what you see? Want to be featured or collaborate with us?
-              Drop us a message and get a chance to work with the team!
-            </p>
-          </div>
-
-          <div
-            style={{
-              minHeight: '100%',
-              paddingLeft: '29px',
-              paddingBottom: '29px',
-            }}
-            class="column has-text-centered is-vcentered">
-            <Link
-              to="/about"
-              class="button "
+            <div
+              class="column is-4 is-offset-1 is-vcentered"
               style={{
-                backgroundColor: 'black',
-                paddingRight: '29px',
-                borderRadius: '10px',
-                padding: '20px',
-                borderColor: 'black',
-                color: 'white',
+                minHeight: '100%',
+                paddingLeft: '29px',
+                color: 'black',
               }}>
-              CONTACT US
-            </Link>
+              <p className="bike">
+                Like what you see? Want to be featured or collaborate with us?
+                Drop us a message and get a chance to work with the team!
+              </p>
+            </div>
+
+            <div
+              style={{
+                minHeight: '100%',
+                paddingLeft: '29px',
+                paddingBottom: '29px',
+              }}
+              class="column has-text-centered is-vcentered">
+              <Link
+                to="/about"
+                class="button "
+                style={{
+                  backgroundColor: 'black',
+                  paddingRight: '29px',
+                  borderRadius: '10px',
+                  padding: '20px',
+                  borderColor: 'black',
+                  color: 'white',
+                }}>
+                CONTACT US
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-  </div>
+      </section>
+    </div>
+  </Layout>
 );
 
 IndexPageTemplate.propTypes = {
